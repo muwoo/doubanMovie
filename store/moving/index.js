12f421f8f5c5183f0d54fe0d8cb120f26dfe424b
monkeyWangs <wang.wei@loonxi.com>
2017-03-15 11:52:16 +0800

    vuex

--- store/moving/index.js
+++ store/moving/index.js
@@ -4,7 +4,7 @@
  */
 import {getters} from './getters'
 import {actions} from './actions'
- import {mutations} from './mutations'
+import {mutations} from './mutations'
 const state = {
   title: 'doubanMovie'
 }
